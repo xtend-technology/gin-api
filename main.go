78c4a83e682b7d5c59a46fe920d94b85e04503c4
Tim <tim@DESKTOP-IFMFRPP.localdomain>
2021-10-18 13:44:10 +1100

    Next change

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	})
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "hello there again!!",
+			"message": "hello there again!!!",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
